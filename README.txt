fe8818fde298972e54b1646846fd8b32d0fb404e
angelo234 <angmatt90@gmail.com>
2021-11-07 20:12:09 -0800

    handles errors and updated README

--- README.txt
+++ README.txt
@@ -1,16 +1,30 @@
-# To create a request to resize image(s), append/create a file called "image_transformer_pipe.txt" and write to a new line:
-IN | (new width), (new height), ("image_filename") | (width2), (height2), ("image_filename2") | ...
+1. Install Python if not installed.
 
-# When the image(s) have been resized, the service will write the names of the files 
-# all appended with "resized" on a new line starting with "OUT":
-OUT | ("resized_image_filename") | ("resized_image_filename2") | ...
-
-# Full Examples: 
-IN | 500,500,"image1.png"
-OUT | "resized_image1.png"
-
-IN | 800,800,"image1.png" | 600,600,"image2.jpg" | 400, 800, "image3.jpeg"
-OUT | "resized_image1.png" | "resized_image2.jpg" | "resized_image3.jpeg"
+2. To start the service (service will keep running and processing images until you Ctrl + C), run the python script with:
+py image_transformer.py
 
 
-IN | 500, 500, "1987-BMW-E30-M3-V13-1080.jpg"
+3. To create a request to resize image(s), append/create a file called "image_transformer_pipe.txt" 
+(don't need to clear file contents as only last line is read) and write to a new line:
+	IN | (new width), (new height), ("image_filename") | (width2), (height2), ("image_filename2") | ...
+
+
+When the image(s) have been resized, the service will write the names of the files 
+all appended with "resized" on a new line starting with "OUT":
+	OUT | ("resized_image_filename") | ("resized_image_filename2") | ...
+
+If you didn't write the request correctly, an error will be outputted to the file.
+
+
+Full Examples: 
+	IN | 500,500,"image1.png"
+	OUT | "resized_image1.png"
+
+	IN | 800,800,"image1.png" | 600,600,"image2.jpg" | 400, 800, "image3.jpeg"
+	OUT | "resized_image1.png" | "resized_image2.jpg" | "resized_image3.jpeg"
+
+	IN | 500, 500, "1987-BMW-E30-M3-V13-1080.jpg"
+	OUT | "resized_1987-BMW-E30-M3-V13-1080.jpg"
+	
+	
+4. To stop the service, press Ctrl + C
--- image_transformer.py
+++ image_transformer.py
@@ -4,44 +4,68 @@ import time
 
 file_path = "image_transformer_pipe.txt"
 
+# Process user requests until they press Ctrl + C
 while True:
-	# Source: https://stackoverflow.com/a/54278929
-	with open('image_transformer_pipe.txt', 'r+') as pipe_file:
-		for line in pipe_file:
-			pass
-		last_line = line
-		
-		data = last_line.split("|")
-		
-		if data[0].strip() == "IN":
-			# Output message string containing resized image filenames	
-			out_str = '\nOUT'
-		
-			end_index = len(data)
+	
+	try:
+		# Source: https://stackoverflow.com/a/54278929
+		with open('image_transformer_pipe.txt', 'r+') as pipe_file:
+			line = ""
 			
-			# Go through the list of images and resize each
-			for i in range(1, end_index):
-				image_data = data[i].split(",")
+			for line in pipe_file:
+				pass
+			last_line = line
+			
+			data = last_line.split("|")
+			
+			# If last line of the file begins with IN, then process the user request
+			if data[0].strip() == "IN":
+				# Output message string containing resized image filenames	
+				out_str = '\nOUT'
+			
+				end_index = len(data)
 				
-				new_width = int(image_data[0])
-				new_height = int(image_data[1])
-				img_name = image_data[2].replace('"', '').lstrip(' ').rstrip(' ')
-				new_img_name = "resized_" + img_name
+				# Go through the list of images and resize each
+				for i in range(1, end_index):
+					image_data = data[i].split(",")
+					
+					new_width = 0
+					new_height = 0
+					img_name = ""
+					new_img_name = ""
+					
+					try:
+						if len(image_data) != 3:
+							out_str += " | ERROR"
+						
+						new_width = int(image_data[0])
+						new_height = int(image_data[1])
+						img_name = image_data[2].replace('"', '').lstrip(' ').rstrip(' ')
+						new_img_name = "resized_" + img_name
+					
+					except:
+						out_str += " | ERROR PARSING USER REQUEST!"
+						break
 
-				# Opens and resizes the image
-				# and creates the new resized image
-				img = Image.open(img_name)	
-				new_img = img.resize((new_width, new_height))
-				new_img.save(new_img_name)
+					try:			
+						# Opens and resizes the image
+						# and creates the new resized image
+						with Image.open(img_name)	as img:
+							new_img = img.resize((new_width, new_height))
+							new_img.save(new_img_name)
+						
+							out_str += ' | "' + new_img_name + '"'
+						
+					except:
+						out_str += " | ERROR RESIZING IMAGE: " + img_name
+						break
+					
+				# Write the outputted resized images
+				pipe_file.write(out_str)
 				
-				out_str += ' | "' + new_img_name + '"'
-				
-			# Write the outputted resized images
-			pipe_file.write(out_str)
-		
+	except FileNotFoundError:
+		pass
 	
 	time.sleep(1)
-	
-	break
 	
 	
